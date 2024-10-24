58e4a198f918f7c68321fa771a46c2ac199462c8
HoangVinh31 <vinh.030104.dh@gmail.com>
2024-10-24 22:59:08 +0700

    add create a post API route

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
+import postRoutes from "./routes/post.route.js";
 
 dotenv.config();
 const app = express();
@@ -20,6 +21,7 @@ mongoose
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+app.use("/api/post", postRoutes);
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
