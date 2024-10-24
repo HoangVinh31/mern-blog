2510065c20cb329cbd0a9f847fa2f1fff043436a
HoangVinh31 <vinh.030104.dh@gmail.com>
2024-10-24 21:37:36 +0700

    add admin functionality to the user

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -21,6 +21,10 @@ const userSchema = new mongoose.Schema(
       default:
         "https://cdn.pixabay.com/photo/2017/06/13/12/54/profile-2398783_1280.png", // Default profile picture if no picture uploaded
     },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
   },
   { timestamps: true }
 );
